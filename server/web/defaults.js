9833d00ce22e3caea3e18e8780cea2e0989daa7b
ari <ari@asu.edu>
2015-09-09 19:23:06 -0700

    refactor2

--- server/web/defaults.js
+++
@@ -1,3 +0,0 @@
-//if(typeof _socket_defaults_js === 'undefined') {
-//    _socket_defaults_js = true;
-
--- server/web/worker.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by ari on 6/19/2015.
- */
-
-importScripts('socket/socket-worker.js');
-importScripts('defaults.js');
