e434c18ee16a05a15c9896bc36aee8f367b5361f
ari <ari@asu.edu>
2015-09-09 22:16:11 -0700

    scripts

---
+++ scripts/start-python-server-8080.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cygstart http://localhost:8080/client/index.html
+python -m SimpleHTTPServer 8080
---
+++ scripts/start-python-server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cygstart http://localhost:80/client/index.html
+python -m SimpleHTTPServer 80
