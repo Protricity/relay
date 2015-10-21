769a4d2bd51433135d8062a9a46e72698ed97fd3
ari <ari@asu.edu>
2015-10-21 10:46:39 -0700

    geoip fix

--- client/tags/render/client-nav-tags.js
+++ client/tags/render/client-nav-tags.js
@@ -12,6 +12,39 @@ if(typeof document === 'object')
         //self.addEventListener('input', onFormEvent);
         //self.addEventListener('change', onFormEvent);
 
+        self._geoipcallback = function(result) {
+            if (result.country_name)
+                result.country = result.country_name;
+            if (result.region_name)
+                result.region = result.region_name;
+            if (result.zip_code)
+                result.postal_code = result.zip_code;
+            if (result.timezone)
+                result.time_zone = result.timezone;
+
+            var channelHTML = "<lh>Recent Commands</lh>";
+            channelHTML += "<li><a href='#JOIN /timezone/" + result.time_zone + "'><span class='command'>Join</span> <strong>" + result.time_zone + "</strong></a></li>";
+            channelHTML += "<li><a href='#JOIN /country/" + result.country_code + "'><span class='command'>Join</span> <strong>" + result.country + "</strong></a></li>";
+            channelHTML += "<li><a href='#JOIN /region/" + result.region_code + "'><span class='command'>Join</span> <strong>" + result.region + "</strong></a></li>";
+            channelHTML += "<li><a href='#JOIN /city/" + result.city + "'><span class='command'>Join</span> <strong>" + result.city + "</strong></a></li>";
+            channelHTML += "<li><a href='#JOIN /zipcode/" + result.postal_code + "'><span class='command'>Join</span> <strong>" + result.postal_code + "</strong></a></li>";
+            channelHTML += "<li><a href='#JOIN /ip/" + result.ip + "'><span class='command'>Join</span> <strong>" + result.ip + "</strong></a></li>";
+
+            document.getElementsByClassName('command-list-recent')[0].innerHTML = channelHTML;
+        };
+
+        var head = document.getElementsByTagName('head')[0];
+
+        var newScript = document.createElement('script');
+        newScript.setAttribute('async', 'async');
+        newScript.setAttribute('src', self.location.protocol + '//www.telize.com/geoip?callback=_geoipcallback');
+        head.appendChild(newScript);
+
+        newScript = document.createElement('script');
+        newScript.setAttribute('async', 'async');
+        newScript.setAttribute('src', 'https://freegeoip.net/json/?callback=_geoipcallback');
+        head.appendChild(newScript);
+
     })();
 
 
@@ -27,51 +60,7 @@ module.exports.renderNavTag = function(tagHTML, callback, Client) {
     xhr.send();
     if(xhr.status !== 200)
         throw new Error("Error: " + xhr.responseText);
-        callback(xhr.responseText);
 
-    var done = false;
-    setTimeout(function() {
-
-        self.geoipcallback = function(result) {
-            if(done)
-                return;
-            done = true;
-
-            if (result.country_name)
-                result.country = result.country_name;
-            if (result.region_name)
-                result.region = result.region_name;
-            if (result.zip_code)
-                result.postal_code = result.zip_code;
-            if (result.timezone)
-                result.time_zone = result.timezone;
-
-            var channelHTML = "<li><a href='#JOIN /country/" + result.country_code + "'><span class='command'>Join</span> <strong>" + result.country + "</strong></a></li>";
-            channelHTML += "<li><a href='#JOIN /region/" + result.region_code + "'><span class='command'>Join</span> <strong>" + result.region + "</strong></a></li>";
-            channelHTML += "<li><a href='#JOIN /city/" + result.city + "'><span class='command'>Join</span> <strong>" + result.city + "</strong></a></li>";
-            channelHTML += "<li><a href='#JOIN /zipcode/" + result.postal_code + "'><span class='command'>Join</span> <strong>" + result.postal_code + "</strong></a></li>";
-//                channelList.innerHTML+= "<li><a href='#JOIN /timezone/" + result.time_zone + "'><span class='command'>Join</span> <strong>" + result.time_zone + "</strong></a></li>";
-            channelHTML += "<li><a href='#JOIN /ip/" + result.ip + "'><span class='command'>Join</span> <strong>" + result.ip + "</strong></a></li>";
-
-            //channelHTML = "<div class='append'>" + channelHTML + "</div>";
-
-            Client.appendChild('command-list-recent', channelHTML);
-        };
-
-        //if(!done) importScripts(self.location.protocol + '//www.telize.com/geoip?callback=geoipcallback');
-        //if(!done) importScripts('https://freegeoip.net/json/?callback=geoipcallback');
-
-    }, 5000);
-
-
+    callback(xhr.responseText);
     return true;
-
-
-    //<!--<li><a href='#JOIN ~'       ><span class='command'>Join ~</span> <span class='info'>(Your channel)</span></a></li>-->\n\
-
-    //<script src="http://www.telize.com/geoip?callback=geoipcallback" async="async" defer="defer"></script>
-    //<script src="https://freegeoip.net/json/?callback=geoipcallback" async="async" defer="defer"></script>
-    //setTimeout()
-
-
 };
