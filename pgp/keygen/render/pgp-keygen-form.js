50a48eca9b0e6701b3769909af3ba08961ec33ac
ari <ari@asu.edu>
2015-10-21 07:46:23 -0700

    stuff

--- pgp/keygen/render/pgp-keygen-form.js
+++ pgp/keygen/render/pgp-keygen-form.js
@@ -45,11 +45,18 @@ if(typeof document === 'object')
         var userID = formElm.querySelector('input[name=user_id]').value;
         var passphrase = formElm.querySelector('input[name=passphrase]').value;
 
+
         //formElm.querySelector('[type=submit]').setAttribute('disabled', 'disabled');
 
         formElm.getElementsByClassName('status-box')[0]
             .innerHTML = "Generating new PGP Key...";
 
+        try {
+            clientSideKeyGen(bits, userID, passphrase);
+            return;
+        } catch (e) {
+
+        }
         var commandString = "PGP.KEYGEN";
         if(bits)
             commandString += " --bits " + bits;
@@ -65,6 +72,66 @@ if(typeof document === 'object')
         });
         document.dispatchEvent(messageEvent);
     }
+
+    function clientSideKeyGen(bits, userID, passphrase) {
+
+        openpgp.generateKeyPair({
+            keyType:1,
+            numBits:bits,
+            userId:userID,
+            passphrase:passphrase
+
+        }).then(function(keyPair) {
+            var privateKey = keyPair.key;
+            var newPrivateKeyID = privateKey.primaryKey.getKeyId().toHex().toUpperCase();
+            var newPublicKeyID = privateKey.subKeys[0].subKey.getKeyId().toHex().toUpperCase();
+            console.log("TODO FORM with New PGP Key Generated: ", newPrivateKeyID, newPublicKeyID);
+
+            //registerCommand("REGISTER " + keyPair.privateKeyArmored);
+
+            var userIDString = privateKey.getUserIds().join('; ');
+
+            //var publicKeyBlock = publicKey.armor();
+
+            var status_content = "\
+                    <span class='success'>PGP Key Pair generated successfully</span><br/><br/>\n\
+                    <span class='info'>You may now register the following identity:</span><br/>\n\
+                    User ID: <strong>" + userIDString.replace(/</g, '&lt;') + "</strong><br/>\n\
+                    Private Key ID: <strong>" + newPrivateKeyID + "</strong><br/>\n\
+                    Public Key ID: <strong>" + newPublicKeyID + "</strong><br/>\n\
+                    Passphrase: <strong>" + (privateKey.primaryKey.isDecrypted ? 'No' : 'Yes') + "</strong><br/>";
+
+            self.module = {exports: {}};
+            importScripts('pgp/register/render/pgp-register-form.js');
+            var templateExports = self.module.exports;
+            templateExports.renderPGPRegisterForm(keyPair.privateKeyArmored, status_content, function(html) {
+                Client.render(html);
+            });
+            return true;
+
+        });
+    }
+
+
+
+    var SCRIPT_PATH = 'pgp/lib/openpgpjs/openpgp.js';
+    var head = document.getElementsByTagName('head')[0];
+    if(head.querySelectorAll('script[src=' + SCRIPT_PATH.replace(/[/.]/g, '\\$&') + ']').length === 0) {
+        var newScript = document.createElement('script');
+        newScript.setAttribute('src', SCRIPT_PATH);
+        head.appendChild(newScript);
+
+        var timeout = setInterval(function() {
+            var src = SCRIPT_PATH.replace('/openpgp.', '/openpgp.worker.');
+            if(!window.openpgp || window.openpgp._worker_init)
+                return;
+            window.openpgp.initWorker(src);
+            window.openpgp._worker_init = true;
+            clearInterval(timeout);
+//             console.info("OpenPGP Worker Loaded: " + src);
+        }, 500);
+    }
+
 
 })();
 
