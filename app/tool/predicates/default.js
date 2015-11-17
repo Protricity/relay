1f0f04edf54ac619b58c5dad1b17407b87dd7244
ari <ari@asu.edu>
2015-11-16 22:38:15 -0700

    remove warning

---
+++ app/tool/predicates/default.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ari on 11/16/2015.
+ */
+
+
+if(typeof module === 'object') (function() {
+    module.exports.getDefaultPredicateList = function (callback) {
+        callback([
+            ["Unleash Democracy"]
+        ])
+    };
+})();
