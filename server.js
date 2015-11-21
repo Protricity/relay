e2ca772f408f0aca98a718be73afeb0aff43e0bb
ari <ari@asu.edu>
2015-11-20 21:22:36 -0700

    larger about

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
  * Created by ari on 9/21/2015.
  */
 
-
+//http://www.chovy.com/web-development/self-signed-certs-with-secure-websockets-in-node-js/
 require('./server/http/http-server.js').HTTPServer.startServer('7315, 80');
 require('./server/socket/socket-server.js').SocketServer.startServer(7314);
