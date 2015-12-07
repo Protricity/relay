1cb9d97350664aac2dbb9785fae9a16afac684a9
ari <ari@asu.edu>
2015-12-06 20:48:06 -0700

    contact list

--- pgp/contact/render/pgp-contact-list.js
+++ pgp/contact/render/pgp-contact-list.js
@@ -54,42 +54,63 @@ if(typeof module === 'object') (function() {
         var KeySpaceDB = self.module.exports.KeySpaceDB;
 
         var status_box = '';
-        var html_contact_entries = '';
+        var html_private_key_entries = '';
+        var html_public_key_entries = '';
+        var html_channel_entries = '';
 
-        // TODO: show public keys, private keys, active channels...
+        var html_command_options = '';
 
         // Query public keys
         var path = 'public/id';
-        var count = 0;
         KeySpaceDB.queryAll(path, function(err, contentEntry) {
             if(err)
                 throw new Error(err);
 
             if(contentEntry) {
-
-                count++;
                 renderPGPContactListEntry(contentEntry, function(html) {
-                    html_contact_entries += html;
+                    html_public_key_entries += html;
+                    html_public_key_entries += html;
+                    html_public_key_entries += html;
+                    html_public_key_entries += html;
+                    html_public_key_entries += html;
                 });
 
             } else {
-                if(count === 0)
-                    status_box = (status_box ? status_box + "<br/>" : '') + "<strong>No PGP Identities found</strong><br/>" +
-                        "<span class='info'>You may <a href='#KEYGEN'>Generate</a>  a new PGP Key Pair Identity</span>";
 
-                var TEMPLATE_URL = "pgp/contact/render/pgp-contact-list.html";
+                var path = '.private/id';
+                KeySpaceDB.queryAll(path, function(err, contentEntry) {
+                    if(err)
+                        throw new Error(err);
 
-                var xhr = new XMLHttpRequest();
-                xhr.open("GET", TEMPLATE_URL, false);
-                xhr.send();
-                if(xhr.status !== 200)
-                    throw new Error("Error: " + xhr.responseText);
-                callback(xhr.responseText
-                    .replace(/{\$status_box}/gi, status_box || '')
-                    .replace(/{\$html_contact_entries}/gi, html_contact_entries || '')
-                );
+                    if(contentEntry) {
+                        renderPGPContactListEntry(contentEntry, function(html) {
+                            html_private_key_entries += html;
+                        });
+
+                    } else {
+
+                        var TEMPLATE_URL = "pgp/contact/render/pgp-contact-list.html";
+
+                        var xhr = new XMLHttpRequest();
+                        xhr.open("GET", TEMPLATE_URL, false);
+                        xhr.send();
+                        if(xhr.status !== 200)
+                            throw new Error("Error: " + xhr.responseText);
+                        callback(xhr.responseText
+                                .replace(/{\$status_box}/gi, status_box || '')
+                                .replace(/{\$html_private_key_entries}/gi, html_private_key_entries || '')
+                                .replace(/{\$html_public_key_entries}/gi, html_public_key_entries || '')
+                                .replace(/{\$html_channel_entries}/gi, html_channel_entries || '')
+                                .replace(/{\$html_command_options}/gi, html_command_options || '')
+                        );
+                                //status_box = (status_box ? status_box + "<br/>" : '') + "<strong>No PGP Identities found</strong><br/>" +
+                                //    "<span class='info'>You may <a href='#KEYGEN'>Generate</a>  a new PGP Key Pair Identity</span>";
+
+                    }
+                });
             }
         });
+
 
 
         return true;
@@ -97,36 +118,26 @@ if(typeof module === 'object') (function() {
 
     module.exports.renderPGPContactListEntry = renderPGPContactListEntry;
     function renderPGPContactListEntry(contentEntry, callback) {
-        var CONTACT_TEMPLATE_ENTRY = "\
-        <div class='pgp-contact-entry:'>\
-            <label>\n\
-                <fieldset class='pgp-id-box pgp-id-box:{$id_public}'>\n\
-                    <legend class='title'>\n\
-                        <input type='checkbox' value='{$id_public}' name='selected:{$id_public}'/> <span class='user'>{$user_id}</span>\n\
-                    </legend>\n\
-                    <strong>Public ID:&nbsp;&nbsp;</strong> <span class='fingerprint'>{$id_public}</span><br/>\n\
-                    <strong>User ID:&nbsp;&nbsp;&nbsp;&nbsp;</strong> <span class='user'>{$user_id}</span><br/>\n\
-                </fieldset>\n\
-            </label>\
-        </div>\
-";
+        var TEMPLATE_URL = "pgp/contact/render/pgp-contact-list-entry.html";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", TEMPLATE_URL, false);
+        xhr.send();
+        if(xhr.status !== 200)
+            throw new Error("Error: " + xhr.responseText);
+
+        // TODO: cache xhr
+
+        var status = 'online';
+        var user_icon_path = 'pgp/contact/render/icons/user_icon_default.png';
 
         // Callback
-        callback(CONTACT_TEMPLATE_ENTRY
-                //.replace(/{\$id_private}/gi, contentEntry.pgp_id_private)
+        callback(xhr.responseText
                 .replace(/{\$id_public}/gi, contentEntry.pgp_id_public)
-                //.replace(/{\$id_private_short}/gi, contentEntry.pgp_id_private.substr(contentEntry.pgp_id_private.length - 8))
                 .replace(/{\$id_public_short}/gi, contentEntry.pgp_id_public.substr(contentEntry.pgp_id_public.length - 8))
-                //.replace(/{\$block_private}/gi, privateKeyBlock)
-                //.replace(/{\$block_public}/gi, privateKeyData.block_public)
-                .replace(/{\$user_id}/gi, contentEntry.user_id.replace(/</g, '&lt;'))
-                //.replace(/{\$user_name}/gi, privateKeyData.user_name || '')
-                //.replace(/{\$user_email}/gi, privateKeyData.user_email || '')
-                //.replace(/{\$user_comment}/gi, privateKeyData.user_comment || '')
-                //.replace(/{\$passphrase_required}/gi, contentEntry.passphrase_required ? "Yes" : "No")
-            //.replace(/{\$is_default}/gi, privateKeyData.default === '1' ? "<strong>Yes</strong>" : "No")
-            //.replace(/{\$class}/gi, privateKeyData.default === '1' ? " pgp-id-box-default" : "")
-            //.replace(/{\$default}/gi, privateKeyData.default)
+                .replace(/{\$name}/gi, contentEntry.user_id.replace(/</g, '&lt;'))
+                .replace(/{\$status}/gi, status)
+                .replace(/{\$user_icon_path}/gi, user_icon_path)
         );
     }
 })();
