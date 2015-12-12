183a40c72f678ad2f1fc373f4d5834e041dd91fb
ari <ari@asu.edu>
2015-12-11 21:19:50 -0700

    server fix

--- server/socket/socket-server-command-proxies.js
+++ server/socket/socket-server-command-proxies.js
@@ -12,7 +12,7 @@ module.exports.initSocketServerCommandProxies = function(SocketServer) {
     //});
     SocketServer.addCommand(httpCommand);
     function httpCommand(commandString, client) {
-        if(!/^(?:keyspace\.)?(get|post|put|delete|patch|head|http|host)/i.test(commandString))
+        if(!/^(?:keyspace\.)?(get|post|put|delete|patch|head|http|host|contacts)/i.test(commandString))
             return false;
         SocketServer.removeCommand(httpCommand);
 
