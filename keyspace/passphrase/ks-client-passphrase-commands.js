3956dc1123247571dec0d8b2a2ba3a3684bacdd5
Ari Asulin <ari.asulin@gmail.com>
2016-01-02 13:14:49 -0700

    Added comments

--- keyspace/passphrase/ks-client-passphrase-commands.js
+++ keyspace/passphrase/ks-client-passphrase-commands.js
@@ -1,10 +1,26 @@
 /**
- * Created by ari.
+ * KeySpace Passphrase Prompt Handler
+ * 
+ * Provides clien-side command handling for KEYSPACE.PASSPHRASE
  */
+ 
+ 
 if(typeof module === 'object') (function() {
+    
+    /**
+     * Initiates Client Command/Response Handlers for the worker thread
+     **/
     module.exports.initClientKSPassphraseCommands = function (ClientWorkerThread) {
+        
+        // Add Command/Response Handlers   
         ClientWorkerThread.addCommand(ksPassphraseCommand);
 
+        /**
+         * Handles Command: KEYSPACE.PASSPHRASE [passphrase]
+         * @param {string} commandString The command string to process 
+         * @param {object} e event The command event
+         * @return {boolean} true if handled otherwise false
+         **/
         function ksPassphraseCommand(commandString, e) {
             var match = /^(?:keyspace\.)?pass(?:phrase)?\s+([a-f0-9 ]{8,})/i.exec(commandString);
             if (!match)
