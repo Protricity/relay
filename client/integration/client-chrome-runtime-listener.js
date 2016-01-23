8e23b9e105fb7afbfbf870e380aa1d254fd2b051
ari <ari@asu.edu>
2016-01-22 23:05:05 -0700

    integration

--- client/integration/client-chrome-runtime-listener.js
+++ client/integration/client-chrome-runtime-listener.js
@@ -1,3 +1,56 @@
+/**
+ * Created by ari on 6/19/2015.
+ */
+
+
+if(typeof document === 'undefined')
+    throw new Error("Invalid Environment");
+
+if(!chrome || !chrome.runtime)
+    throw new Error("Missing: chrome.runtime");
+
+if(!Client || !Client.execute)
+    throw new Error("Missing: Client.execute");
+
+(function() {
+
+    var activeClientPorts = [];
+    function addListener(port) {
+        if(port.name !== name)
+            throw new Error("Unrecognized Port Name: " + port.name);
+
+        activeClientPorts.push(port);
+        console.log("New Port Client: " + name, port);
+        port.onMessage.addListener(
+            function (message) {
+                console.log("Executing Proxy Message: ", message, port);
+                Client.execute(message, port);
+            }
+        );
+    }
+
+    chrome.runtime.onConnect.addListener(addListener);
+    console.log("Port Listener loaded: " + name);
+
+    Client.processResponse = function(responseString) {
+        var args = /^\w+/.exec(responseString);
+        if (!args)
+            throw new Error("Invalid Command: " + responseString);
+
+        // Handle port hosting
+        for (i = 0; i < activeClientPorts.length; i++) {
+            var port = activeClientPorts[i];
+            try {
+                port.postMessage(responseString);
+
+            } catch (e) {
+                activeClientPorts.splice(i--, 1);
+                console.info("Removed disconnected Port");
+            }
+        }
+
+    };
+})();
 
 //Client.tryConnectToPortListener = function(name) {
 //    if(socketWorker)
