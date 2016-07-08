c7c8fceb70cc9731a43f62d9d5d547d13523fb65
Ari Asulin <ari.asulin@gmail.com>
2016-07-08 00:39:53 -0700

    install js

---
+++ install.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ari on 7/7/2016.
+ */
+
+
+console.log("Setting up Relay Server Dependencies...");
+
+var exec = require('child_process').execSync;
+
+
+exec('npm config set registry http://registry.npmjs.org/');
+exec('npm install --save ws');
+exec('npm install --save openpgp');
+exec('npm install mongodb');
+// exec('npm install indexeddb-js sqlite3');
+// exec('npm install http-server -g');
+
+console.log("Finished. Try '$ node server.js' to test server");
