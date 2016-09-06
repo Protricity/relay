f9e9e7a1c2116587cf59812063738b0b8474b1d2
Ari Chromebook Asulin <ari.asulin@gmail.com>
2016-09-05 23:32:27 -0700

    worker refactor

--- client-worker.js
+++ client-worker.js
@@ -4,28 +4,46 @@
 
 if(typeof document !== 'undefined') {
 
-    var socketWorker = new Worker('client-worker.js');
-
-    socketWorker.addEventListener('message', function(e) {
-        var responseString = e.data || e.detail || e;
-        Client.processResponse(responseString);
-    }, true);
-
     if(typeof self.Client === 'undefined')
         self.Client = {};
+        
+    if(true && typeof SharedWorker == 'function') {
 
-    Client.execute = function (commandString) {
-        socketWorker.postMessage(commandString);
-    };
+        
+        var sharedSocketWorker = new SharedWorker('client-worker.js');
 
-    //Client.processResponse = Client.processResponse || function (responseString) {
-    //    throw new Error("Client must define Client.processResponse = function(responseString)...")
-    //};
+        sharedSocketWorker.port.addEventListener('message', function(e) {
+            var responseString = e.data || e.detail || e;
+            Client.processResponse(responseString);
+        }, true);
+        sharedSocketWorker.port.start();
 
-    console.log("Started Client WebWorker", socketWorker);
+        Client.execute = function (commandString) {
+            sharedSocketWorker.port.postMessage(commandString);
+        };
+
+        console.log("Started Client SharedWorker", sharedSocketWorker);
+    } else if (typeof Worker == 'function') {
+        var socketWorker = new Worker('client-worker.js');
+
+        socketWorker.addEventListener('message', function(e) {
+            var responseString = e.data || e.detail || e;
+            Client.processResponse(responseString);
+        }, true);
+
+        Client.execute = function (commandString) {
+            socketWorker.postMessage(commandString);
+        };
+
+        console.log("Started Client WebWorker", socketWorker);
+
+    } else {
+        throw new Error("Worker unavailable");
+    }
 
 } else if(typeof importScripts !== 'undefined') {
     importScripts('client/worker/client-worker-thread.js');
+    console.log("Initiated WebWorker thread");
 
 } else {
     throw new Error("Invalid Environment");
