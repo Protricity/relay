ace5f4c80ebaeb1ce2e637999ddf457e63f9a5e1
Ari Chromebook Asulin <ari.asulin@gmail.com>
2016-09-05 22:41:23 -0700

    combined install into server.js

--- server.js
+++ server.js
@@ -2,10 +2,23 @@
  * Created by ari on 9/21/2015.
  */
 
-//http://www.chovy.com/web-development/self-signed-certs-with-secure-websockets-in-node-js/
-var defaults = require('./config.js');
+console.log("Setting up Relay server dependencies...");
 
-require('./server/http/http-server.js')
-    .HTTPServer.startServer(defaults.HTTP_SERVER_DEFAULT_PORT);
-require('./server/socket/socket-server.js')
-    .SocketServer.startServer(defaults.SOCKET_SERVER_DEFAULT_PORT);
+var exec = require('child_process').exec;
+if(!exec)
+	throw new Error('Could not fork child process ;(');
+
+exec('npm config set registry http://registry.npmjs.org/');
+exec('npm install --save ws');
+exec('npm install --save openpgp');
+exec('npm install mongodb');
+
+
+console.log("Testing dependencies...");
+var defaults = require('./config.js');
+var HTTPServer = require('./server/http/http-server.js').HTTPServer;
+var SocketServer = require('./server/socket/socket-server.js').SocketServer;
+
+console.log("Starting Server...");
+HTTPServer.startServer(defaults.HTTP_SERVER_DEFAULT_PORT);
+SocketServer.startServer(defaults.SOCKET_SERVER_DEFAULT_PORT);
